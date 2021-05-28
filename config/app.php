1227e3254bc17e24871ed3fadfb46f9a3d8706ed
Charles Awulor <c.awulor@yahoo.com>
2021-05-28 07:54:42 +0100

    adjustment

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class,
+      //  Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,7 +229,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Cart' => Gloudemans\Shoppingcart\Facades\Cart::class,
+     //   'Cart' => Gloudemans\Shoppingcart\Facades\Cart::class,
 
     ],
 
